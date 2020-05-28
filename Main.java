e94aba6549931dc91a4e5962ef9023e860ea7cd6
swallace23 <65972366+swallace23@users.noreply.github.com>
2020-05-28 01:06:32 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("wanna fuck lol");
+        System.out.println("input yes or no");
+
+        String answer = in.nextLine();
+
+        if (answer.equals("yes")) {
+            System.out.println("tight");
+        } else {
+            System.out.println("fuck u lol");
+        }
+    }
+}
